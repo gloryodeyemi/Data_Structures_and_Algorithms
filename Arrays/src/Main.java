3b3608ffb1fe9e3f1640b51f3e8ce55f875b1f7d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-12-14 18:17:37 -0500

    find index of integer in array

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -7,8 +7,19 @@ public class Main {
         intArray[3] = 90;
         intArray[4] = -50;
 
+        // print out integers in the array
         for (int i=0; i< intArray.length; i++){
             System.out.println("Integer in index " + i + " = " + intArray[i]);
         }
+
+        // find index of -4
+        int index = -1;
+        for (int i=0; i< intArray.length; i++){
+            if (intArray[i] == -4){
+                index = i;
+                break;
+            }
+        }
+        System.out.println("Index of -4 = " + index);
     }
 }
