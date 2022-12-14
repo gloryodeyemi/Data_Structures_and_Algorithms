1979bcdfa72615fdb24c618c0e6dcab89137fd8a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-12-14 17:59:40 -0500

    simple integer array

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] intArray = new int[5];
+        intArray[0] = 10;
+        intArray[1] = 20;
+        intArray[2] = -4;
+        intArray[3] = 90;
+        intArray[4] = -50;
+
+        for (int i=0; i< intArray.length; i++){
+            System.out.println("Integer in index " + i + " = " + intArray[i]);
+        }
     }
 }
