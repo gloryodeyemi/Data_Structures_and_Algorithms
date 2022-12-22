5b026d41228700de9c71064fa8229dc09dec32a0
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-12-21 20:07:34 -0500

    bubble sort

---
+++ BubbleSort/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, -22, 12};
+
+        // bubble sort algorithm
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            for (int i=0; i < lastUnsortedIndex; i++){
+                if (intArray[i] > intArray[i + 1]){
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        // print out sorted array
+        for (int item : intArray) {
+            System.out.println(item);
+        }
+
+    }
+
+    // method to swap items in the array
+    public static void swap(int[] array, int i, int j){
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
